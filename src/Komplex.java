992734c364046c971578a453cd05753be80c1e1c
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-03-16 13:21:41 +0100

    Initial commit

---
+++ src/Komplex.java
@@ -0,0 +1,13 @@
+public class Komplex {
+    double re;
+    double im;
+
+    public double getAddi() {
+        return re+im;
+    }
+
+    public double getmulti() {
+        return re*im;
+    }
+
+}
